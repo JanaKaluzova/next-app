49459e41afaa465adb315dae6492b66020fb6e63
Jana Kaluzova <jani.kaluzova@gmail.com>
2022-09-26 15:12:00 +0200

    filter by status

---
+++ components/Filter.tsx
@@ -0,0 +1,38 @@
+import Link from "next/link";
+import { TaskStatus } from "../generated/graphql-frontend";
+
+interface Props {
+  status?: TaskStatus;
+}
+
+export const Filter: React.FC<Props> = ({ status }) => {
+  return (
+    <ul className="task-filter">
+      <li>
+        <Link href="/" scroll={false}>
+          <a className={!status ? "task-filter-active" : ""}>All</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/[status]" as="/active" scroll={false} shallow={true}>
+          <a
+            className={status === TaskStatus.Active ? "task-filter-active" : ""}
+          >
+            Active
+          </a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/[status]" as="/completed" scroll={false} shallow={true}>
+          <a
+            className={
+              status === TaskStatus.Completed ? "task-filter-active" : ""
+            }
+          >
+            Completed
+          </a>
+        </Link>
+      </li>
+    </ul>
+  );
+};
